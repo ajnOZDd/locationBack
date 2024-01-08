ec25e28b4e194c541850703a8be6b055240373c9
ajnOZDd <allantohaina2@mail.com>
2024-01-08 21:56:33 +0100

    creation repository back

---
+++ src/main/java/com/back/locationback/LocationbackApplication.java
@@ -0,0 +1,13 @@
+package com.back.locationback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationbackApplication.class, args);
+	}
+
+}
